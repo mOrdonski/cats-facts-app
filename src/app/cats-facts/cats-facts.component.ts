72a0ae903f29b49a99e1ceba589056d36e108766
mOrdonski <maciejordonski@gmail.com>
2022-06-01 22:16:36 +0200

    cats-facts module

---
+++ src/app/cats-facts/cats-facts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cats-facts',
+  templateUrl: './cats-facts.component.html',
+  styleUrls: ['./cats-facts.component.scss']
+})
+export class CatsFactsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/cats-facts/cats-facts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CatsFactsComponent } from './cats-facts.component';
+
+
+
+@NgModule({
+  declarations: [
+    CatsFactsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CatsFactsModule { }
