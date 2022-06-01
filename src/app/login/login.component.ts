5005a786bed7cbfee46a06c8ad592b383411a4b5
mOrdonski <maciejordonski@gmail.com>
2022-06-01 22:12:42 +0200

    login method

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,23 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
   hide: boolean = true;
 
   logInForm: FormGroup = new FormGroup({
     username: new FormControl(''),
     password: new FormControl(''),
   });
+
+  logIn() {
+    this.authService.accounts.find((account) => {
+      if (
+        account.username === this.logInForm.value.username &&
+        account.password === this.logInForm.value.password
+      ) {
+        console.log('logged in');
+      }
+    });
+  }
 }
